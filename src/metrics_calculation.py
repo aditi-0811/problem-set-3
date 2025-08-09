4b2c818ba021d1abc45606c6a0bf515f8fd4e887
Aditi Kulkarni <akulka08@umd.edu>
2025-08-09 19:39:51 -0400

    Setup main.py and preprocessing&eval modules

--- src/metrics_calculation.py
+++ src/metrics_calculation.py
@@ -4,6 +4,8 @@ PART 2: METRICS CALCULATION
 - Write the functions below
     - Further info and hints are provided in the docstrings
     - These should return values when called by the main.py
+
+NOTE: Added if __ > 0 else 0 because I got division by 0 errors when I first ran main.py
 '''
 
 import numpy as np
@@ -37,10 +39,37 @@ def calculate_metrics(model_pred_df, genre_list, genre_true_counts, genre_tp_cou
     Hint #2: Micro metrics are tuples, macro metrics are lists
 
     '''
-
     # Your code here
+    sum_tp = sum(genre_tp_counts.values())
+    sum_fp = sum(genre_fp_counts.values())
+    #Actual calculation for false negative (genre present - model correctly guessed genre = model missed genre)
+    sum_fn = sum(genre_true_counts[g] - genre_tp_counts[g] for g in genre_list)
 
-    
+    #Calculate micro precision, recall and F1-score
+    micro_precision = sum_tp / (sum_tp + sum_fp) if (sum_tp + sum_fp) > 0 else 0
+    micro_recall = sum_tp / (sum_tp + sum_fn) if (sum_tp + sum_fn) > 0 else 0
+    micro_f1 = (2 * micro_precision * micro_recall) / (micro_precision + micro_recall) if (micro_precision + micro_recall) > 0 else 0
+
+    # Calculate macro metrics per genre, append as lists
+    macro_prec_list = []
+    macro_recall_list = []
+    macro_f1_list = []
+
+    for g in genre_list:
+        tp = genre_tp_counts[g]
+        fp = genre_fp_counts[g]
+        fn = genre_true_counts[g] - tp
+
+        precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+        recall = tp / (tp + fn) if (tp + fp) > 0 else 0
+        f1 = (2 * precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+
+        macro_prec_list.append(precision)
+        macro_recall_list.append(recall)
+        macro_f1_list.append(f1)
+
+    return micro_precision, micro_recall, micro_f1, macro_prec_list, macro_recall_list, macro_f1_list
+
 def calculate_sklearn_metrics(model_pred_df, genre_list):
     '''
     Calculate metrics using sklearn's precision_recall_fscore_support.
@@ -62,3 +91,26 @@ def calculate_sklearn_metrics(model_pred_df, genre_list):
     '''
 
     # Your code here
+    pred_rows = []
+    true_rows = []
+
+    #append 
+    for _, row in model_pred_df.iterrows():
+        actual = row['actual genres']
+        predicted = row['predicted']
+        
+        #Makes it binary for sk-learn
+        true_row = [1 if g in actual else 0 for g in genre_list]
+        pred_row = [1 if g in predicted else 0 for g in genre_list]
+
+        true_rows.append(true_row)
+        pred_rows.append(pred_row)
+
+    true_matrix = pd.DataFrame(true_rows)
+    pred_matrix = pd.DataFrame(pred_rows)
+
+    #Calculate the metrics with sklearn and this output is called in main.py
+    macro_prec, macro_rec, macro_f1, _ = precision_recall_fscore_support(true_matrix, pred_matrix, average='macro', zero_division=0)
+    micro_prec, micro_rec, micro_f1, _ = precision_recall_fscore_support(true_matrix, pred_matrix, average='micro', zero_division=0)
+
+    return macro_prec, macro_rec, macro_f1, micro_prec, micro_rec, micro_f1
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -7,6 +7,7 @@ PART 1: PRE-PROCESSING
 '''
 
 import pandas as pd
+import ast
 
 def load_data():
     '''
@@ -17,7 +18,10 @@ def load_data():
         genres_df (pd.DataFrame): DataFrame containing genre information
     '''
     # Your code here
+    model_pred_df = pd.read_csv('/Users/Aditi/Documents/UMD/Classes/INST414-Data_Science_Techniques/Problem Sets/problem-set-3/data/prediction_model_03.csv')
+    genres_df = pd.read_csv('/Users/Aditi/Documents/UMD/Classes/INST414-Data_Science_Techniques/Problem Sets/problem-set-3/data/genres.csv')
 
+    return model_pred_df, genres_df
 
 def process_data(model_pred_df, genres_df):
     '''
@@ -28,6 +32,44 @@ def process_data(model_pred_df, genres_df):
         genre_true_counts (dict): Dictionary of true genre counts
         genre_tp_counts (dict): Dictionary of true positive genre counts
         genre_fp_counts (dict): Dictionary of false positive genre counts
-    '''
 
+    '''
     # Your code here
+
+    #List of unique genres
+    genre_list = genres_df['genre'].tolist()
+
+    ##Setup predicted and actual genres lists for dictionary initialization
+    #Kept as series of lists
+    model_pred_df['predicted'] = model_pred_df["predicted"].apply(lambda x: [x])
+    model_pred_df["actual genres"] = model_pred_df["actual genres"].apply(ast.literal_eval)
+
+    #Dictionary of true genre counts:
+    genre_true_counts = {
+        g: 0 for g in genre_list
+    }
+
+    #Dictionary of true positive genre counts:
+    genre_tp_counts = {
+        g: 0 for g in genre_list
+    }
+    
+    #Dictionary of false positive genre counts
+    genre_fp_counts = {
+        g: 0 for g in genre_list
+    }
+
+    #Initialize the counting for each dictionary: 
+    for _, row in model_pred_df.iterrows():
+        actual = row['actual genres']
+        predicted = row['predicted']
+
+        for g in genre_list:
+            if g in actual:
+                genre_true_counts[g] += 1
+            if g in predicted and g in actual:
+                genre_tp_counts[g] += 1
+            if g in predicted and g not in actual:
+                genre_fp_counts[g] += 1
+  
+    return genre_list, genre_true_counts, genre_tp_counts, genre_fp_counts
